8ee8f39a0a0858b9b8441a712d6771a908601a15
Ben West <bewest+insulaudit@gmail.com>
2013-07-28 09:35:06 -0700

    travis secret update

--- prebuild.sh
+++ prebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "HELLO WORLD [via travis?]"
 echo "GIT_NAME $GIT_NAME"
 echo "GIT_EMAIL $GIT_EMAIL"
+echo "HELLO WORLD [via travis] $GIT_NAME <$GIT_EMAIL>"
 export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
 git config user.email "$GIT_EMAIL"
 git config user.name "$GIT_NAME"
@@ -10,7 +10,6 @@ echo "STATUS"
 git status
 git remote rename origin old
 echo "remotes pre pre-authorized remote url"
-git config deployRemote $REPO_URL
 git remote -v
 git remote add origin $REPO_URL
 
